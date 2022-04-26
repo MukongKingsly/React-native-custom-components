b6c0570315badfb018ccd0958644f9179f0b013e
Kingsly Mbaaka <kingmbamu@yahoo.co.uk>
2022-04-26 22:26:32 +0200

    Dialog

---
+++ src/dialog/Dialog.js
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import RNModal from "react-native-modal";
+import ThemeContext from "../Theme/ThemeContext";
+
+export default function Dialog({ children, isOpen, onRequestClose, style }) {
+  const theme = useContext(ThemeContext);
+
+  const backgroundColor = theme?.colors?.background || "#fff";
+
+  return (
+    <RNModal
+      style={styles.container}
+      coverScreen={true}
+      isVisible={isOpen}
+      swipeDirection="down"
+      onBackButtonPress={onRequestClose}
+      onBackdropPress={onRequestClose}
+      onSwipeComplete={onRequestClose}
+      propagateSwipe
+    >
+      <View style={[styles.modal, { backgroundColor }, style]}>{children}</View>
+    </RNModal>
+  );
+}
+
+Dialog.propTypes = {
+  children: PropTypes.node,
+  isOpen: PropTypes.bool,
+  onRequestClose: PropTypes.func,
+  style: PropTypes.any,
+};
+
+Dialog.defaultProps = {
+  children: null,
+  isOpen: false,
+  onRequestClose: null,
+  style: {},
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    margin: 0,
+  },
+  modal: {
+    margin: 32,
+    padding: 16,
+    borderRadius: 2,
+  },
+});
